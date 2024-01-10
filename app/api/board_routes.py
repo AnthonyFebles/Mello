33a3c430bdee8c8922ffd5f746dfb1fa20009ba1
PorkyJames <kwanjko17@gmail.com>
2024-01-09 17:54:39 -0800

    Created boilerplate Board routes

--- app/api/board_routes.py
+++ app/api/board_routes.py
@@ -1,1 +1,32 @@
-from flask import Flask
+from flask import Blueprint
+
+bp = Blueprint('boards', __name__)
+
+
+# GET ROUTE (Read)
+# Logged in User can view all of their boards
+@bp.route("/session")
+def get_boards():
+    pass
+
+
+# POST ROUTE (Create)
+# Logged in User can create a board
+@bp.route("/boards")
+def post_boards():
+    pass
+
+
+# UPDATE ROUTE (Update)
+# Logged in User can update any of their boards
+@bp.route("/boards/:boardId")
+def update_boards():
+    pass
+
+
+# DELETE ROUTE (Delete)
+# Logged in User can delete any of their boards
+@bp.routes("/boards/:boardId")
+def delete_boards():
+    pass
+

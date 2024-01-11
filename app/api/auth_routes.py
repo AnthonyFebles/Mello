34ec79acb4246c117e4dcbda166bb0eeb7975eac
AnthonyFebles <anthonyfebles21@outlook.com>
2024-01-10 20:10:04 -0500

    trying to add back the updated user model, backend routes boilerplate, boards model, updated seeds, and updated piplock and updated gitignore pt2

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,10 +1,21 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify, session,
 from app.models import User, db
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
 
 auth_routes = Blueprint('auth', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
 
 
 @auth_routes.route('/')
@@ -31,7 +42,7 @@ def login():
         user = User.query.filter(User.email == form.data['email']).first()
         login_user(user)
         return user.to_dict()
-    return form.errors, 401
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
 @auth_routes.route('/logout')
@@ -60,7 +71,7 @@ def sign_up():
         db.session.commit()
         login_user(user)
         return user.to_dict()
-    return form.errors, 401
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
 @auth_routes.route('/unauthorized')

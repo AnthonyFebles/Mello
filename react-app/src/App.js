e370ba022fe51929b36c165bb6036c2c35c69958
Rodrick Z. <116767605+rodrickCode97@users.noreply.github.com>
2024-01-10 18:22:41 -0600

    dev branch first commit

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -21,7 +21,7 @@ function App() {
           <Route path="/login" >
             <LoginFormPage />
           </Route>
-          <Route path="/signup">
+          <Route  path="/signup">
             <SignupFormPage />
           </Route>
         </Switch>

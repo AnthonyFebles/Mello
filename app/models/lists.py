ca596394ce19dd36aa14a295a16c207887409e7b
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-01-11 18:28:49 -0700

    tweaked the relationships in the models and added "add_prefix_for_prod" to the foreign keys and added a to_dict to the models

--- app/models/lists.py
+++ app/models/lists.py
@@ -3,15 +3,24 @@ from datetime import datetime
 
 class List(db.Model):
     __tablename__ = 'lists'
-    
+
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-#  Foreign Keys
-    board_id = db.Column(db.Integer, db.ForeignKey("boards.id"))
+    board_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("boards.id")))
     name = db.Column(db.String(80), Nullable = False)
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
     board = db.relationship("Board", back_populates="lists")
+    cards = db.relationship("Card", back_populates="list")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'board_id': self.board_id,
+            'name': self.name,
+            'board': self.board,
+            'cards': self.cards
+        }

2dda191ac85372934e356b1a205d63734fd2a608
Rodrick Z. <116767605+rodrickCode97@users.noreply.github.com>
2024-01-12 19:29:40 -0600

    completed routes for lists

--- app/api/list_routes.py
+++ app/api/list_routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, redirect
+from app.forms import ListForm
+from app.models import db, List
+from .auth_routes import validation_errors_to_error_messages
 
 bp = Blueprint("lists", __name__)
 
@@ -7,25 +10,50 @@ bp = Blueprint("lists", __name__)
 # Logged in User can create a List on their Board
 @bp.route("/boards/<int:board_id>/lists", methods=["POST"])
 def create_lists(board_id):
-    pass
+    form = ListForm()
+    if form.validate_on_submit():
+        board_id = board_id
+        name = form.name.data
+        new_list = List(board_id=board_id, name=name)
+        db.session.add(new_list)
+        db.commit()
+        return new_list.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+  
+
 
 
 # Read Route
 # Logged in User can view a List on their Board
 @bp.route("/boards/<int:board_id>/lists")
 def read_lists(board_id):
-    pass
+    lists = List.query.all(board_id)
+    if lists:
+        return {'lists': [list.to_dict() for list in lists]}
+    return 'Board list not found'
+
 
 
 # Update Route
 # Logged in User can update a List on their Board
 @bp.route("/lists/<int:list_id>", methods=["PUT"])
 def update_lists(list_id):
-    pass
+    form = ListForm()
+    list = List.query.get(List.id == list_id)
+    if list:
+        if form.validate_on_submit():
+            name = form.name.data
+            form.name = name
+            db.commit()
+            return list.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
 
 
 # Delete Route
 # Logged in User can delete a List on their Board
 @bp.route("/lists/<int:list_id>", methods=["DELETE"])
 def delete_lists(list_id):
-    pass
+    list = List.filter(List.id == list_id).delete()
+
+    db.session.commit()

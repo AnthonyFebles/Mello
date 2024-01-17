7faeb676c18edcf574892667013f67925eec8acb
AnthonyFebles <anthonyfebles21@outlook.com>
2024-01-16 21:05:18 -0500

    turning off join turn seeds for now

--- app/models/users.py
+++ app/models/users.py
@@ -3,6 +3,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from datetime import datetime
 
+shared_boards = db.Table('shared_boards', db.Model.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
+    db.Column('board_id', db.Integer, db.ForeignKey(add_prefix_for_prod('boards.id')), primary_key=True)
+)
+
+user_cards = db.Table('user_cards', db.Model.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True),
+    db.Column('card_id', db.Integer, db.ForeignKey(add_prefix_for_prod('cards.id')), primary_key=True)
+)
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -18,7 +27,9 @@ class User(db.Model, UserMixin):
     hashed_password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
-    boards = db.relationship('Board', back_populates='users', cascade='all, delete-orphan')
+    used_boards = db.relationship('Board', secondary=shared_boards ,back_populates='users')
+    owned_boards = db.relationship('Board', back_populates='owner', cascade='all, delete-orphan')
+    cards = db.relationship('Card', secondary=user_cards, back_populates='users')
     comments = db.relationship('Comment', back_populates='users', cascade='all, delete-orphan')
 
     @property
@@ -38,5 +49,8 @@ class User(db.Model, UserMixin):
             'first_name': self.first_name,
             'last_name':self.last_name,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'used_boards': self.used_boards,
+            'owned_boards': self.owned_boards,
+            'comments': self.comments,
         }

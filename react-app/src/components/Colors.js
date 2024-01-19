4f109ff21e890f3d797f969eef4d9e5b29c4b055
AnthonyFebles <anthonyfebles21@outlook.com>
2024-01-18 23:25:54 -0500

    got rid of warning messages and added some comments to create_board component

--- react-app/src/components/Colors.js
+++ react-app/src/components/Colors.js
@@ -1,3 +1,5 @@
+// Where an array of themes urls can be stored
+
 export const colors = [
 	"https://th.bing.com/th/id/OIG.OoOd9Dks6SQIeJc3lV_8?w=1024&h=1024&rs=1&pid=ImgDetMain",
 	"https://th.bing.com/th/id/OIG.Tm4j5l5hso8iB85_iqNf?w=1024&h=1024&rs=1&pid=ImgDetMain",
@@ -13,5 +15,5 @@ export function getRandomInt(max) {
 
 export const random = getRandomInt(colors.length);
 
-const randomColor = colors[random]
+
 

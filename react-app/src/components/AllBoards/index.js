80be8da5642872ebe45d38b7295705e12b690c27
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-01-20 11:53:22 -0700

    added a comments redux store

--- react-app/src/components/AllBoards/index.js
+++ react-app/src/components/AllBoards/index.js
@@ -3,7 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { getBoards } from "../../store/boards";
 import { NavLink } from "react-router-dom";
 import "./AllBoards.css";
+<<<<<<< HEAD
 import NewBoard from "../CreateBoard";
+=======
+>>>>>>> 13b3df6 (all-boards-component partially done, logic is there)
 
 const AllBoards = () => {
 
@@ -25,22 +28,36 @@ const AllBoards = () => {
 		return null;
 	}
 
+<<<<<<< HEAD
     
 
 	// console.log(boards, "board")
+=======
+	// console.log(boards, "board")
+    let source_color_image;
+>>>>>>> 13b3df6 (all-boards-component partially done, logic is there)
 
 	return (
 		<>
 			<h1>Hello From All Boards</h1>
+<<<<<<< HEAD
 			<div className="tabbed-nav__container">
 				<div className="tabbed-nav__group">
+=======
+			<div class="tabbed-nav__container">
+				<div class="tabbed-nav__group">
+>>>>>>> 13b3df6 (all-boards-component partially done, logic is there)
 					
 						<NavLink to={`/boards`} className={"tabbed-nav__link"} id="boards-tab">
 							Boards
 						</NavLink>
 					
 
+<<<<<<< HEAD
 					<a className="tabbed-nav__link" id="settings-tab" href=" ">
+=======
+					<a class="tabbed-nav__link" id="settings-tab">
+>>>>>>> 13b3df6 (all-boards-component partially done, logic is there)
 						{/* Insert page for editing user details here  */}
 						Settings
 					</a>
@@ -48,9 +65,26 @@ const AllBoards = () => {
 			</div>
 
 			<div className="boards__container">
+<<<<<<< HEAD
 
                 <NewBoard/>
 				
+=======
+				<div class="boards__sidebar">
+					<div class="boards__sidebar-content">
+						<p class="title">Need unlimited boards?</p>
+						<p class="description">
+							Upgrade to Business Class to get unlimited Boards and Power-ups,
+							automation, and much more.
+						</p>
+						<p class="cta">
+							<a href="" className="learn-more-link">
+								Learn More
+							</a>
+						</p>
+					</div>
+				</div>
+>>>>>>> 13b3df6 (all-boards-component partially done, logic is there)
 				<div className="boards__group">
 					{boards.toReversed().map((board) => {
 						return (
@@ -64,12 +98,21 @@ const AllBoards = () => {
 									className={`board-${board.color} boards__img__navlink`}
 								>
 									<p className="board__name">{`${board.name}`}</p>
+<<<<<<< HEAD
 									
 									<img
 										src={`${board.color}`}
 										alt="Board "
 										className={`board-${board.color} boards__img`}
 										title={`${board.name}`}
+=======
+
+									<img
+										src={`${board.color}`}
+										alt="Board Preview Image"
+										className={`board-${board.color} boards__img`}
+										title={`${board.description}`}
+>>>>>>> 13b3df6 (all-boards-component partially done, logic is there)
 									/>
 								</NavLink>
 							</div>

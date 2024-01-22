98b99b4a9a58a828956a81012f386bdc83eb57d7
AnthonyFebles <anthonyfebles21@outlook.com>
2024-01-22 16:10:06 -0500

    fixing constraints for board color

--- app/models/boards.py
+++ app/models/boards.py
@@ -10,7 +10,7 @@ class Board(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False, name='boards_user_id_constraint')
-    color = db.Column(db.String(30), nullable=True)
+    color = db.Column(db.String(255), nullable=True)
     name = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)

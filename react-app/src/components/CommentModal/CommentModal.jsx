ae5730717f72e7887c6afb17ba7decc4dccdb6d0
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-01-21 23:28:20 -0700

    working on the comments store and the comments modal

--- react-app/src/components/CommentModal/CommentModal.jsx
+++ react-app/src/components/CommentModal/CommentModal.jsx
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Editor, EditorState, RichUtils } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 import './CommentModal.css';
 
 
 export default function CommentModal({ cardId }) {
-  const [editorState, setEditorState] = React.useState(() =>
+  const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
+  const [editorState2, setEditorState2] = useState(() =>
+    EditorState.createEmpty()
+  );
+  const [clicked, setClicked] = useState(false)
+  const [clicked2, setClicked2] = useState(false)
 
   const onBoldClick = () => {
     setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
   }
+  const onBoldClick2 = () => {
+    setEditorState2(RichUtils.toggleInlineStyle(editorState2, 'BOLD'));
+  }
   const onItalicClick = () => {
     setEditorState(RichUtils.toggleInlineStyle(editorState, 'ITALIC'));
+  }
+  const onItalicClick2 = () => {
+    setEditorState2(RichUtils.toggleInlineStyle(editorState2, 'ITALIC'));
   }
   const onUnderlineClick = () => {
     setEditorState(RichUtils.toggleInlineStyle(editorState, 'UNDERLINE'));
   }
+  const onUnderlineClick2 = () => {
+    setEditorState2(RichUtils.toggleInlineStyle(editorState2, 'UNDERLINE'));
+  }
   const onRedClick = () => {
     setEditorState(RichUtils.toggleInlineStyle(editorState, 'RED'));
   }
+  const onRedClick2 = () => {
+    setEditorState2(RichUtils.toggleInlineStyle(editorState2, 'RED'));
+  }
   const onHighlightClick = () => {
     setEditorState(RichUtils.toggleInlineStyle(editorState, 'HIGHLIGHT'));
+  }
+  const onHighlightClick2 = () => {
+    setEditorState2(RichUtils.toggleInlineStyle(editorState2, 'HIGHLIGHT'));
   }
 
   return (
@@ -34,7 +54,38 @@ export default function CommentModal({ cardId }) {
           <p>In list: list name</p>
         </div>
       </div>
-
+      <div className="description">
+        <div className="row-container">
+          <p><i class="fa-solid fa-align-left fa-lg" style={{color: 'white'}}></i>  Description</p>
+        </div>
+      </div>
+      <div className="editor-container">
+        {clicked && (
+          <div className="toolbar">
+            <button onClick={onBoldClick}><i className="fa-solid fa-bold"></i></button>
+            <button onClick={onItalicClick}><i className="fa-solid fa-italic"></i></button>
+            <button onClick={onUnderlineClick}><i className="fa-solid fa-underline"></i></button>
+            <button onClick={onRedClick}><i className="fas fa-palette" style={{color: 'red'}}></i></button>
+            <button onClick={onHighlightClick}><i className="fa-solid fa-highlighter" style={{color: 'goldenrod'}}></i></button>
+          </div>
+        )}
+        <div style={{ height: clicked ? '200px' : 'initial' }}>
+          <Editor
+            className="editor"
+            onFocus={() => setClicked(true)}
+            editorState={editorState}
+            onChange={setEditorState}
+            placeholder="Add a more detailed description..."
+            customStyleMap={{
+              BOLD: { fontWeight: 'bold' },
+              ITALIC: { fontStyle: 'italic' },
+              UNDERLINE: { textDecoration: 'underline' },
+              RED: { color: 'red' },
+              HIGHLIGHT: { backgroundColor: 'yellow' },
+            }}
+          />
+        </div>
+      </div>
       <div id="activity">
         <div className="row-container">
           <p><i class="fa-sharp fa-regular fa-comments fa-lg" style={{color: 'white'}}></i>  Comments</p>
@@ -42,28 +93,32 @@ export default function CommentModal({ cardId }) {
         </div>
       </div>
       <div className="editor-container">
-        <div className="toolbar">
-          <button onClick={onBoldClick}><i className="fa-solid fa-bold"></i></button>
-          <button onClick={onItalicClick}><i className="fa-solid fa-italic"></i></button>
-          <button onClick={onUnderlineClick}><i className="fa-solid fa-underline"></i></button>
-          <button onClick={onRedClick}><i className="fas fa-palette" style={{color: 'red'}}></i></button>
-          <button onClick={onHighlightClick}><i className="fa-solid fa-highlighter" style={{color: 'goldenrod'}}></i></button>
+        {clicked2 && (
+          <div className="toolbar">
+            <button onClick={onBoldClick2}><i className="fa-solid fa-bold"></i></button>
+            <button onClick={onItalicClick2}><i className="fa-solid fa-italic"></i></button>
+            <button onClick={onUnderlineClick2}><i className="fa-solid fa-underline"></i></button>
+            <button onClick={onRedClick2}><i className="fas fa-palette" style={{color: 'red'}}></i></button>
+            <button onClick={onHighlightClick2}><i className="fa-solid fa-highlighter" style={{color: 'goldenrod'}}></i></button>
+          </div>
+        )}
+        <div style={{ height: clicked2 ? '50px' : 'initial' }}>
+          <Editor
+            className="editor"
+            onFocus={() => setClicked2(true)}
+            editorState={editorState2}
+            onChange={setEditorState2}
+            placeholder="Write a comment..."
+            customStyleMap={{
+              BOLD: { fontWeight: 'bold' },
+              ITALIC: { fontStyle: 'italic' },
+              UNDERLINE: { textDecoration: 'underline' },
+              RED: { color: 'red' },
+              HIGHLIGHT: { backgroundColor: 'yellow' },
+            }}
+          />
         </div>
-        <Editor
-          className="editor"
-          editorState={editorState}
-          onChange={setEditorState}
-          placeholder="Write a comment..."
-          customStyleMap={{
-            BOLD: { fontWeight: 'bold' },
-            ITALIC: { fontStyle: 'italic' },
-            UNDERLINE: { textDecoration: 'underline' },
-            RED: { color: 'red' },
-            HIGHLIGHT: { backgroundColor: 'yellow' },
-          }}
-        />
       </div>
-      <p>{cardId}</p>
     </div>
   );
 }

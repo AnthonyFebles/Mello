bdf8e6b6b3af9cacb46bdbb59f662d1cf30b2249
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-01-26 22:34:57 -0700

    finished styling the 'add a card' feature

--- react-app/src/components/Lists/Cards.jsx
+++ react-app/src/components/Lists/Cards.jsx
@@ -50,54 +50,50 @@ export default function Cards({ listId, boardId }) {
 
   return (
     <>
-    <div className="add-a-card">
-      <div className="card-cover">
-        <div className="card-info">
-          <div onClick={() => setClickedAdd(true)} className="add">
-            {!clickedAdd && (
-              <>
-                <div className="add-card-button">
-                  <i class="fa-solid fa-plus fa-lg"></i>
-                  <p>Add a card</p>
-                </div>
-              </>
-            )}
-            {clickedAdd && (
-              <form className="card-form" onSubmit={handleSubmit}>
-                <div className="new-card-add-info">
-                  <input
-                    className="new-card-name"
-                    name="name"
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    placeholder="Enter a title for this card..."
-                    required
-                  />
-                  <input
-                    className="new-card-description"
-                    id="description"
-                    type="text"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    placeholder="Add a more detailed description..."
-                    required
-                  />
-                </div>
-                <div className="card-form-button">
-                  {errors && <p>{errors.errors}</p>}
-                  <button className="new-card-btn" type="submit">Add Card</button>
-                  <i className="fa-solid fa-xmark fl-lg" onClick={(e) => {
+      <div className="card-info">
+        <div onClick={() => setClickedAdd(true)} className="add">
+          {!clickedAdd && (
+            <>
+              <div className="add-card-button">
+                <i class="fa-solid fa-plus fa-lg"></i>
+                <p>Add a card</p>
+              </div>
+            </>
+          )}
+          {clickedAdd && (
+            <form className="card-form" onSubmit={handleSubmit}>
+              <div className="new-card-add-info">
+                <input
+                  className="new-card-name"
+                  name="name"
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Enter a title for this card..."
+                  required
+                />
+                <input
+                  className="new-card-description"
+                  id="description"
+                  type="text"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  placeholder="Add a description..."
+                  required
+                />
+              </div>
+              <div className="card-form-button">
+                {errors && <p>{errors.errors}</p>}
+                <button className="new-card-btn" type="submit">Add Card</button>
+                <button className="cancel-new-card" type="button" onClick={(e) => {
                     e.stopPropagation()
                     setClickedAdd(false)
-                  } }></i>
-                </div>
-              </form>
-            )}
-          </div>
+                  } }>X</button>
+              </div>
+            </form>
+          )}
         </div>
       </div>
-    </div>
     </>
   )
 }

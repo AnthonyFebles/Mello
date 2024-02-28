f965a2cdf095407e7058f8efeda580b61654e6a1
Rodrick Z. <116767605+rodrickCode97@users.noreply.github.com>
2024-02-27 22:35:25 -0600

    Merge branch 'dev' of https://github.com/AnthonyFebles/Mello into dev

--- react-app/src/components/SpotifyPlayer/SpotifyPlayer.jsx
+++ react-app/src/components/SpotifyPlayer/SpotifyPlayer.jsx
@@ -1,17 +1,37 @@
 import { useState } from "react";
 import Modal from "react-modal";
+import ReactPlayer from "react-player";
 import "./SpotifyPlayer.css";
 
 function SpotifyPlayer() {
 	const [modalIsVisible, setModalIsVisible] = useState(false);
+	const [playing, setPlaying] = useState(false);
+	const [source, setSource] = useState(
+		"https://www.youtube-nocookie.com/embed/fnVlN327sn0?si=lHe1YkNFnLdkUlzN&amp;start=3"
+	);
+	const [playButton, setPlayButton] = useState(
+		"fa-regular fa-circle-play fa-xl"
+	);
+	const [volume, setVolume] = useState(0.5);
 
 	function handleClick() {
 		setModalIsVisible(true);
 	}
 
+	function handlePlay() {
+
+		if (playing) {
+			setPlaying(false) 
+			setPlayButton("fa-regular fa-circle-play fa-xl");
+		} else {;
+			setPlaying(true)
+			setPlayButton("fa-regular fa-circle-pause fa-xl")
+		}
+	}
+
 	return (
 		<>
-			<Modal
+			{/* <Modal
 				className="spotify-modal"
 				isOpen={true}
 				onRequestClose={() => setModalIsVisible(false)}
@@ -33,10 +53,49 @@ function SpotifyPlayer() {
 					allowfullscreen
 				></iframe>
 				<button onClick={() => setModalIsVisible(false)}>Close</button>
+			</Modal> */}
+			<Modal
+				className="spotify-modal"
+				isOpen={true}
+				onRequestClose={() => setModalIsVisible(false)}
+				contentLabel="Spotify Player"
+				style={{
+					overlay: {
+						display: modalIsVisible ? "block" : "none",
+						backgroundColor: "#00436a96",
+						width: "100%",
+					},
+				}}
+			>
+				<>
+					<p style={{ color: "whitesmoke" }}>Enter A Link For Audio</p>
+
+					<input
+						className="audio-input"
+						value={source}
+						onChange={(e) => setSource(e.target.value)}
+					></input>
+					<label style={{ color: "whitesmoke", textAlign: "center" }}>
+						Volume
+						<input type="range" onChange={(e) => setVolume(e.target.value / 100)}></input>
+					</label>
+				</>
 			</Modal>
+			<ReactPlayer
+				url={source}
+				playing={playing}
+				width={"0px"}
+				height={"0px"}
+				volume={volume}
+			/>
 			<i
 				onClick={handleClick}
-				class="fa-regular fa-circle-play fa-xl"
+				class="fa-solid fa-gear fa-xl"
+				style={{ color: "#e6e6fa71" }}
+			></i>
+			<i
+				onClick={handlePlay}
+				class={playButton}
 				style={{ color: "#e6e6fa71" }}
 			></i>
 		</>

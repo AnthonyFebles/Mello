a410109d0eaaf5eef08dae7eb11cd651cd4171e4
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-03-01 18:01:34 -0700

    testing other styling options for lists

--- react-app/src/components/CommentModal/CommentModal.jsx
+++ react-app/src/components/CommentModal/CommentModal.jsx
@@ -184,6 +184,13 @@ export default function CommentModal({
 	}, [dispatch, cardId, id]);
 
 	return (
+		<>
+		{coverColor && (
+			<div
+				className="card-cover-modal"
+				style={{ backgroundColor: `${coverColor}` }}
+			/>
+		)}
 		<div className="commentModal">
 			<div className="commentModal-body">
 				<div className="cardTitle">
@@ -460,5 +467,6 @@ export default function CommentModal({
 				</div>
 			</div> */}
 		</div>
+		</>
 	);
 }

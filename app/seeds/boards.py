0d244b1c538ab32e80697df5b1f8d44fcc34c457
AnthonyFebles <anthonyfebles21@outlook.com>
2024-05-22 22:03:52 -0400

    fixed links for images to a better solution, images also downloaded just in case

--- app/seeds/boards.py
+++ app/seeds/boards.py
@@ -1,18 +1,19 @@
 from sqlalchemy.sql import text
 from app.models import db, Board, environment, SCHEMA
 
+
 def seed_boards():
     board1 = Board(
-        user_id="1", color="https://th.bing.com/th/id/OIG.rt3EmryUoYQKIjK86m_p?pid=ImgGn", name="App Academy"
+        user_id="1", color="https://imgur.com/BdTGQmT.jpeg", name="App Academy"
     )
     board2 = Board(
-        user_id="2", color="https://th.bing.com/th/id/OIG.fEHxWkIYkumMxQZLmYc5?w=1024&h=1024&rs=1&pid=ImgDetMain", name="board_2"
+        user_id="2", color="https://imgur.com/LCD7Cke.jpeg", name="board_2"
     )
     board3 = Board(
-        user_id="3", color="https://th.bing.com/th/id/OIG.Tm4j5l5hso8iB85_iqNf?w=1024&h=1024&rs=1&pid=ImgDetMain", name="board_3"
+        user_id="3", color="https://imgur.com/A4Y76t2.jpeg", name="board_3"
     )
     board4 = Board(
-        user_id="1", color="https://th.bing.com/th/id/OIG.idCzopGsrbq9HoGGWuLq?w=1024&h=1024&rs=1&pid=ImgDetMain", name="Home"
+        user_id="1", color="https://imgur.com/Gaf7yTG.jpeg", name="Home"
     )
 
     db.session.add(board1)
@@ -20,6 +21,7 @@ def seed_boards():
     db.session.add(board3)
     db.session.add(board4)
     db.session.commit()
+
 
 def undo_boards():
     if environment == "production":
